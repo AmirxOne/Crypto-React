ed164c1c7c18a909916cc56dc6f22a4cacf77374
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-06-06 11:16:10 +0430

    Delete image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Lander from "./Component/Lander";
 import Style from "./Component-Style/App.module.css"
-import background from "../../first-app/src/image/bg-quickstart.dab977c4.png"
 
 const App = () => {
     return (
         <div className={Style.App}>
             <Lander/>
-            <img src={background} alt="background"/>
         </div>
     );
 };
